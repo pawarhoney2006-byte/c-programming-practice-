0f388ab223861dada8bacd6d9fc04f1c30475bc2
Jyoti Pawar <pawarhoney2006@gmail.com>
2026-01-15 21:59:29 +0530

    Add files via upload

---
+++ armstrong_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,r,sum=0,temp;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    temp=n;
+    while(n>0){
+        r=n%10;
+        sum = sum + (r*r*r);
+        n=n/10;
+    }
+    if(temp==sum){
+    printf("The number is Armstrong");
+    }else{
+    printf("The number is not Armstrong");}
+    return 0;
+}
